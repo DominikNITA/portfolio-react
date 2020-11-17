4b9c1593779270e69600e5f1730228c246b6a516
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2020-11-17 23:54:45 +0100

    Updates

---
+++ precache-manifest.e0daa4671e03ccaa019498d8b4562e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cb450bd6a82ad41989ba7a0230f585",
+    "url": "/portolio-react/index.html"
+  },
+  {
+    "revision": "7b51a1f84a0406f31571",
+    "url": "/portolio-react/static/css/main.9c398696.chunk.css"
+  },
+  {
+    "revision": "c340a8d8249c1c1cb94f",
+    "url": "/portolio-react/static/js/2.4acaf265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portolio-react/static/js/2.4acaf265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b51a1f84a0406f31571",
+    "url": "/portolio-react/static/js/main.d0474442.chunk.js"
+  },
+  {
+    "revision": "90fcf59c4ab184887f8e",
+    "url": "/portolio-react/static/js/runtime-main.837277b2.js"
+  }
+]);
